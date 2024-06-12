d6dccb5da11e1a873410947fd2713ef1d7eb3fdc
Nicolas Llancaqueo <n.llancaqueo01@ufromail.cl>
2024-06-12 14:57:25 -0400

    Create repo

---
+++ src/main/java/com/taller28/taller28/Taller28Application.java
@@ -0,0 +1,13 @@
+package com.taller28.taller28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller28Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/taller28/taller28/Taller28ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller28.taller28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
